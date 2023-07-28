bee20523df9a1906e7b030ca77635a80d23efee4
Althaf <althafnaa@gmail.com>
2023-07-28 16:16:15 +0700

    edited vision scaling

--- modules/landing/CompanyVision.tsx
+++ modules/landing/CompanyVision.tsx
@@ -3,7 +3,7 @@ import styles from "./CompanyVision.module.css"
 
 const CompanyVision: FC = memo(() => (
   <div className="">
-    <div className="container mx-auto flex flex-col xl:px-60">
+    <div className="container mx-auto flex flex-col xl:px-80">
       <div className="self-start m-5">
         <div
           className={`mt-5 my-5 flex relative pr-12 bg-blue-500 w-fit rounded-full ${styles.bg_title}`}
@@ -11,10 +11,10 @@ const CompanyVision: FC = memo(() => (
           {/* <div
           className={`absolute w-full h-full opacity-20 mr-8 bg-yellow-400 rounded-full ${styles.bg_title}`}
         ></div> */}
-          <div className={`w-16 h-16 rounded-full my-auto mr-6 ${styles.red_balls}`}></div>
-          <h2 className={`text-5xl my-auto font-bold ${styles.vision_title}`}>Inclusive</h2>
+          <div className={`w-12 h-12 rounded-full my-auto mr-6 ${styles.red_balls}`}></div>
+          <h2 className={`text-4xl my-auto font-bold ${styles.vision_title}`}>Inclusive</h2>
         </div>
-        <p className={`${styles.vision_text} text-3xl text-left ml-8 mr-2`}>
+        <p className={`${styles.vision_text} text-xl text-left ml-8 mr-2`}>
           CEO FTUI akan bersikap inklusif kepada seluruh mahasiswa dengan mengenalkan berbagai
           perusahaan dalam bidang-bidang yang berbeda
         </p>
@@ -25,10 +25,10 @@ const CompanyVision: FC = memo(() => (
         {/* <div
           className={`absolute w-full h-full opacity-20 mr-8 bg-yellow-400 rounded-full ${styles.bg_title}`}
         ></div> */}
-        <h2 className={`text-5xl my-auto font-bold ${styles.vision_title}`}>Intuitive</h2>
-        <div className={`w-16 h-16 rounded-full my-auto ml-6 ${styles.red_balls}`}></div>
+        <h2 className={`text-4xl my-auto font-bold ${styles.vision_title}`}>Intuitive</h2>
+        <div className={`w-12 h-12 rounded-full my-auto ml-6 ${styles.red_balls}`}></div>
       </div>
-      <p className={`${styles.vision_text} text-3xl self-end text-right mr-14 ml-6`}>
+      <p className={`${styles.vision_text} text-xl self-end text-right mr-14 ml-6`}>
         Mahasiswa diharapkan dapat mendengarkan intuisi mereka selama kegiatan CEO FTUI untuk
         memilih jalan pasca sarjana yang akan ditempuh
       </p>
@@ -40,10 +40,10 @@ const CompanyVision: FC = memo(() => (
           {/* <div
           className={`absolute w-full h-full opacity-20 mr-8 bg-yellow-400 rounded-full ${styles.bg_title}`}
         ></div> */}
-          <div className={`w-16 h-16 rounded-full my-auto mr-6 ${styles.red_balls}`}></div>
-          <h2 className={`text-5xl my-auto font-bold ${styles.vision_title}`}>Pro-Active</h2>
+          <div className={`w-12 h-12 rounded-full my-auto mr-6 ${styles.red_balls}`}></div>
+          <h2 className={`text-4xl my-auto font-bold ${styles.vision_title}`}>Pro-Active</h2>
         </div>
-        <p className={`${styles.vision_text} text-3xl text-left ml-8 mr-2`}>
+        <p className={`${styles.vision_text} text-xl text-left ml-8 mr-2`}>
           Ketika kegiatan CEO FTUI, diharapkan mahasiswa dapat bersikap proactive dengan ilmu yang
           diberikan dalam kesempatan ini.
         </p>
